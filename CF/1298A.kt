eb5306b545b40ae24610822169dcd537a9800b56
Hichem Razgallah <hichemraz@gmail.com>
2020-02-26 08:17:59 +0100

    New Problems, Solve it in Kotlin

--- CF/1298A.kt
+++ CF/1298A.kt
@@ -1,3 +1,7 @@
 fun main() {
-	println("Hello World!")
+	val nums = readLine()!!.split(" ").map { it.toInt()}.sorted()
+	val num1 = nums[3] - nums[2]
+	val num2 = nums[3] - nums[1]
+	val num3 = nums[3] - nums[0]
+	println("${num1} ${num2} ${num3}")
 }
